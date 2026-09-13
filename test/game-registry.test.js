53e196073107c8385c8a716f02f898e89085f3e1
Dinh Nam <hainam7076@gmail.com>
2026-09-13 16:23:51 +0700

    Normalize invalid package directory errors

--- test/game-registry.test.js
+++ test/game-registry.test.js
@@ -30,4 +30,5 @@ test('registry rejects non-string IDs and identifies the invalid package public 
   assert.throws(() => createGameRegistry([{ ...game(), id: 123 }], { exists: () => true }), /invalid game id/);
   assert.throws(() => createGameRegistry([{ ...game(), publicDir: new URL('../package.json', import.meta.url).pathname }]), /alpha\.publicDir/);
   assert.throws(() => createGameRegistry([{ ...game(), publicDir: 'public' }]), /alpha\.publicDir/);
+  assert.throws(() => createGameRegistry([{ ...game(), publicDir: '/invalid\0directory' }]), /alpha\.publicDir must be an existing absolute public directory/);
 });
